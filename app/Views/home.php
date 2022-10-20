a8ce88c8d828ca3be62a5219105326c06176ba81
nabs <nabilakiswanto@gmail.com>
2022-10-20 10:45:30 +0800

    catweb app

---
+++ app/Views/home.php
@@ -0,0 +1,4 @@
+<?= $this->extend('inc/template'); ?>
+<?= $this->section('content'); ?>
+<h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et metus fringilla, tincidunt lorem a, porta ligula. Curabitur a justo erat.</h1>
+<?= $this->endSection(); ?>
